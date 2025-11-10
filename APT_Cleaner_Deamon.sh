459f27f6e27245697002dfb3ed570285dd41ad04
Mole <157280874+MoleSof@users.noreply.github.com>
2025-11-10 11:11:02 -0700

    Update APT_Cleaner_Deamon.sh

--- APT_Cleaner_Deamon.sh
+++ APT_Cleaner_Deamon.sh
@@ -28,3 +28,16 @@ $sudo apt check
 $sudo apt modernize-sources
 $sudo apt held
 #$sudo apt
+
+
+
+####What the fuck is wrong with APT working oddly with $?
+sudo dpkg --configure -a
+$sudo apt install -f
+$sudo apt install --fix-broken
+sudo dpkg --configure -a
+####What the fuck is wrong with APT working oddly with $?
+$sudo apt install -f
+$sudo apt install --fix-broken
+$sudo apt purge
+####add -y and complete with error but print make deb version without the I wanan say check option?
