2a05e03830c574257e22accf7810f7fc558a6287
Mole <157280874+MoleSof@users.noreply.github.com>
2025-11-10 10:45:18 -0700

    Create APT_Cleaner_Deamon.sh

---
+++ APT_Cleaner_Deamon.sh
@@ -0,0 +1,30 @@
+#Apt Cleaner Demon#
+#run once a week
+#
+#run once a day
+#
+#run once an hour
+#
+#run once a month
+$sudo apt update
+$sudo apt autoremove
+$sudo apt auto-remove
+$sudo apt autopurge
+$sudo apt autoclean
+#$sudo apt fix-
+$sudo apt clean
+$sudo apt check
+$sudo apt modernize-sources
+$sudo apt held
+#$sudo apt
+$sudo apt update
+$sudo apt autoremove
+$sudo apt auto-remove
+$sudo apt autopurge
+$sudo apt autoclean
+#$sudo apt fix-
+$sudo apt clean
+$sudo apt check
+$sudo apt modernize-sources
+$sudo apt held
+#$sudo apt
