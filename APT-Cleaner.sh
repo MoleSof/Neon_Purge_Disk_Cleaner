2489fc2e3debbdeeae2d4b4b922aceeab3a6225b
Mole <157280874+MoleSof@users.noreply.github.com>
2025-11-10 10:44:17 -0700

    Create APT-Cleaner.sh

---
+++ APT-Cleaner.sh
@@ -0,0 +1,23 @@
+#Apt Cleaner Demon without autorun#
+$sudo apt update
+$sudo apt autoremove
+$sudo apt auto-remove
+$sudo apt autopurge
+$sudo apt autoclean
+#$sudo apt fix-
+$sudo apt clean
+$sudo apt check
+$sudo apt modernize-sources
+$sudo apt held
+#$sudo apt
+$sudo apt update
+$sudo apt autoremove
+$sudo apt auto-remove
+$sudo apt autopurge
+$sudo apt autoclean
+#$sudo apt fix-
+$sudo apt clean
+$sudo apt check
+$sudo apt modernize-sources
+$sudo apt held
+#$sudo apt
